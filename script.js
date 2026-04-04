676d7f86b7c7b235dfd54c957ea1cf0a77f207db
Ansh Pandey <anshpandey7836@example.com>
2026-04-04 14:16:39 +0530

    bahut kuch kiya lekin smjh kuch nhi aya

--- script.js
+++ script.js
@@ -2,6 +2,15 @@
 const tabButtons = document.querySelectorAll('.tab-button');
 const tabPanes = document.querySelectorAll('.tab-pane');
 
+// Get clock elements
+const clocktime = document.getElementById('clock-time');
+const clockdate = document.getElementById('clock-date');
+const clockperiod = document.getElementById('clock-period');
+const toggle= document.getElementById('formate-toggle');
+const formatelabel = document.getElementById('formate-label');
+
+let hours12 = true; // Default to 24-hour format
+
 
 // Add click event listener to each tab button
 tabButtons.forEach(button => {
@@ -17,5 +26,28 @@ tabButtons.forEach(button => {
         document.getElementById(tabName).classList.add('active');
     });
 });
-
-
+// clock time
+function pad(n) {
+    return String(n).padStart(2, '0');
+    //iska kaam hai ki agar n 1 digit ka hai to uske aage 0 add kar dega 
+    // yani use 05 ke form me convert kar dega
+}
+const updateClock = () => {
+    const now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let period = '';
+  clocktime.innerText = `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+  clockperiod.innerText = period;
+  clockperiod.style.display = hours12 ? 'inline' : 'none';
+  let nowDate = now.toLocaleString('en-US', { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' });
+  clockdate.innerText = nowDate;
+// toggle.addEventListener('click', () => {
+//     hour12 = !hour12;
+//     toggleFormatButton.textContent = hour12 ? 'Switch to 24-Hour Format' : 'Switch to 12-Hour Format';
+//     formatLabel.textContent = hour12 ? '24-Hour Format' : '12-Hour Format';
+// });
+}
+updateClock();
+setInterval(updateClock, 1000);
