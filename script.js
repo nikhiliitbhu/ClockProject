a0997f3fa725536560e80e15ad7829dc405da884
shubhamtiwari <shubham.tiwari925748@gmail.com>
2026-04-04 14:27:25 +0530

    commit1

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Get all tab buttons and tab panes
-const tabButtons = document.querySelectorAll('.tab-button');
-const tabPanes = document.querySelectorAll('.tab-pane');
+let tabButtons = document.querySelectorAll('.tab-button');
+let tabPanes = document.querySelectorAll('.tab-pane');
 
 
 // Add click event listener to each tab button
 tabButtons.forEach(button => {
     button.addEventListener('click', function() {
-        const tabName = this.getAttribute('data-tab');
+        let tabName = this.getAttribute('data-tab');
         
         // Remove active class from all buttons and panes
         tabButtons.forEach(btn => btn.classList.remove('active'));
@@ -17,5 +17,20 @@ tabButtons.forEach(button => {
         document.getElementById(tabName).classList.add('active');
     });
 });
+let clockTime = document.getElementById('clock-time');
+let clockDate = document.getElementById('clock-date');
+let period = document.getElementById('clock-period');
+let timeformat = document.getElementById('timeformat');
+function storeTime(){
+    let timeDate = ((new Date()).toString());
+    let time = timeDate.slice(16,24);
+    let date = timeDate.slice(0,15);
+    let hour = timeDate.slice(16,17);
+    (hour>12) ? "PM" : "AM";
+    clockTime.innerText = time;
+    clockDate.innerText = date;
+
+}
+setInterval(storeTime,1000);
 
 
