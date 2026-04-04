6ff33f426c6d978dc112491bbd2fc3b4a2433c5d
shubhamkumar <shubhamkumar971191@gmail.com>
2026-04-04 14:26:51 +0530

    clock change

--- script.js
+++ script.js
@@ -19,3 +19,30 @@ tabButtons.forEach(button => {
 });
 
 
+
+function updateTime(){
+    let now = new Date();
+    let hour=now.getHours();
+    let minute=now.getMinutes();
+    let second=now.getSeconds();
+
+    let time = hour +":"+ minute+":"+ second;
+    document.getElementById('clock-time').innerText= time;
+    
+
+}
+setInterval(updateTime,1000);
+updateTime();
+
+
+
+function updateDate(){
+ let now= (new Date()).toString().slice(0,15);
+ document.getElementById("clock-date").innerText= now;
+ 
+}
+updateDate();
+
+function updateclockperiod(){
+    let now=(new Date()).toString().slice()
+}
