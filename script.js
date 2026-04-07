cef1b456b4893c8c5a9f82d4dec1e1764fb5eeb3
nikhil <nksingh7000@gmail.com>
2026-04-07 13:25:44 +0530

    complete implementation of stopwatch

--- script.js
+++ script.js
@@ -19,3 +19,76 @@ tabButtons.forEach(button => {
 });
 
 
+//STOPWATCH
+
+let display = document.getElementById('stopwatch-time');
+let pauseBtn = document.getElementById('stopwatch-pause');
+let startBtn = document.getElementById('stopwatch-start');
+let resetBtn = document.getElementById('stopwatch-reset');
+let lapBtn = document.getElementById('stopwatch-lap');
+let lapList = document.getElementById('lap-list');
+
+let start = null, id;
+let lapData = [];
+
+function startStopwatch(){
+    startBtn.disabled = true;
+    pauseBtn.disabled = false;
+    resetBtn.disabled = false;
+    lapBtn.disabled = false;
+
+    start = Date.now();
+    id = setInterval(updateStopwatch, 1000);
+}
+
+function updateStopwatch(){
+    let ms = Date.now() - start;
+    let currentStopwatchTime = Math.floor(ms/3600000);
+    ms -= currentStopwatchTime*3600000;
+    currentStopwatchTime += ":" + Math.floor(ms/60000) + ":";
+    ms -= Math.floor(ms/60000)*60000;
+    currentStopwatchTime += Math.floor(ms/1000);
+
+    display.innerText = currentStopwatchTime;
+}
+
+function resetStopwatch(){
+    clearInterval(id);
+    display.innerText = "00:00:00";
+    lapList.innerHTML = null;
+
+    startBtn.disabled = false;
+    pauseBtn.disabled = true;
+    resetBtn.disabled = true;
+    lapBtn.disabled = true;
+}
+
+function pauseStopwatch(){
+
+    if(pauseBtn.innerText === "PAUSE"){
+        clearInterval(id);
+        pauseTime = Date.now();
+        pauseBtn.innerText = "PLAY";
+        pauseBtn.style.backgroundColor = "green";
+
+    } else {
+        start += Date.now() - pauseTime;
+        id = setInterval(updateStopwatch, 1000);
+        pauseBtn.innerText = "PAUSE";
+        pauseBtn.style.backgroundColor = "#FFA500";
+
+    }
+}
+
+function lap(){
+    lapData.push(display.innerText);
+    let lapTable = "<table>  <tr> <th> Index </th> <th> Record </th> </tr>  ";
+
+    for(let i = lapData.length-1; i >= 0 ; i--){ 
+        lapTable += `<tr> <td> ${i+1} </td> <td> ${lapData[i]} </td></tr>`;
+    }
+
+    lapTable += " </table>";
+
+    lapList.innerHTML = lapTable;
+}
